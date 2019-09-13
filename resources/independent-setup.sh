decb88a743bfb856b5c08ed88da11e6aebf473a1
Alex Snow <asnow@ignw.io>
2019-09-13 16:19:25 -0700

    Bedrock servers now supported

---
+++ resources/independent-setup.sh
@@ -0,0 +1,431 @@
+echo '${data.template_file.deploy.rendered}' > resources/mc-pod-provisioned.yaml 
+gcloud container clusters get-credentials ${var.cluster-name} --zone ${var.region}-a --project ${var.project}
+kubectl apply -f resources/mc-pod-provisioned.yaml 
+rm resources/mc-pod-provisioned.yaml
+echo '${data.template_file.server-properties.rendered}' > ./resources/server.properties.provisioned 
+gcloud container clusters get-credentials ${var.cluster-name} --zone ${var.region}-a --project ${var.project} 
+sleep 60 && kubectl cp ./resources/server.properties.provisioned ${lookup("${var.pod-names}", "${var.java}")}:/data/server.properties 
+kubectl exec -it ${lookup("${var.pod-names}", "${var.java}")} chmod 777 /data/server.properties 
+rm ./resources/server.properties.provisioned
+
+
+kubectl exec -it ${lookup("${var.pod-names}", "${var.java}")} rcon-cli stop
+
+#-or-
+
+kubectl exec ${lookup("${var.pod-names}", "${var.java}")} -- /bin/sh -c 'kill 1'
+
+
+
+
+
+#!/bin/bash
+# base code from https://sookocheff.com/post/bash/parsing-bash-script-arguments-with-shopts/
+
+gamemode="0"  # Default gamemode
+worldname="k8s"  # Default worldname
+release="1.14.4"
+modpath=""
+worldtype=DEFAULT
+modpack=""
+server-type=VANILLA
+
+# Parse options to the `mc-server` command
+while getopts ":hc:bg:w:v:m:f:r" opt; do
+  case ${opt} in
+   h )
+     echo "
+Usage:
+
+-c Name of the cluster to set this up in
+
+-r Sets up a Bedrock server, ignoring these options: -vmbf
+-g Gamemode of the server(0 or 1 on Java; survival or creative on Bedrock)
+-w Worldname of the server
+-v Version of Minecraft to use
+-m Activates Forge; path to the mod file(.jar) required
+-b Creates a Biomes 'O' Plenty world if -m is also called and the modpath points to the Biomes 'O' Plenty mod file
+-f Activates FTB; URL or path of modpack required
+
+Note: Make sure the modpacks and mods match the version of Minecraft under the -v flag
+Other Note: Using both -m and -f will only activate -m
+" 1>&2
+     exit 1
+     ;;
+   c )
+     clustername=$OPTARG
+     ;;
+   r )
+     bedrock=true
+     ;;
+   g )
+     gamemode=$OPTARG 
+     ;;
+   w )
+     worldname=$OPTARG 
+     ;;
+   v )
+     release=$OPTARG
+     ;;
+   m )
+     modded=true
+     server-type=FORGE
+     modpath=$OPTARG
+     ;;
+   b )
+     worldtype=BIOMESOP
+     ;;
+   f )
+     ftb=true
+     server-type=FTB
+     modpack=$OPTARG
+     ;;
+   \? )
+     echo "
+Invalid Option: -$OPTARG
+
+Usage:
+
+-c Name of the cluster to set this up in
+
+-r Sets up a Bedrock server, ignoring these options: -vmbf
+-g Gamemode of the server(0 or 1 on Java; survival or creative on Bedrock)
+-w Worldname of the server
+-v Version of Minecraft to use
+-m Activates Forge; path to the mod file(.jar) required
+-b Creates a Biomes 'O' Plenty world if -m is also called and the modpath points to the Biomes 'O' Plenty mod file
+-f Activates FTB; URL or path of modpack required
+
+Note: Make sure the modpacks and mods match the version of Minecraft under the -v flag
+Other Note: Using both -m and -f will only activate -m
+" 1>&2
+     exit 1
+     ;;
+   : )
+     echo "Invalid option: $OPTARG requires an argument" 1>&2
+     exit 1
+     ;;
+  esac
+done
+shift $((OPTIND -1))
+
+
+        echo -e "spawn-protection=16
+max-tick-time=60000
+query.port=25565
+generator-settings=
+force-gamemode=false
+allow-nether=true
+gamemode="${gamemode}"
+enable-query=false
+player-idle-timeout=0
+difficulty=1
+spawn-monsters=true
+op-permission-level=4
+pvp=true
+snooper-enabled=true
+level-type="${worldtype}"
+hardcore=false
+enable-command-block=true
+network-compression-threshold=256
+max-players=20
+resource-pack-sha1=
+max-world-size=29999984
+rcon.port=25575
+server-port=25565
+texture-pack=
+server-ip=
+spawn-npcs=true
+allow-flight=false
+level-name="${worldname}"
+view-distance=10
+displayname=Fill this in if you have set the server to public\!
+resource-pack=
+discoverability=unlisted
+spawn-animals=true
+white-list=false
+rcon.password=minecraft
+generate-structures=true
+online-mode=true
+max-build-height=256
+level-seed=
+use-native-transport=true
+prevent-proxy-connections=false
+motd=A Minecraft Server powered by K8S
+enable-rcon=true" > resources/server.properties.provisioned
+
+echo 'kind: Pod
+apiVersion: v1
+metadata:
+  name: mc-server-pod-java
+  labels: 
+    app: java
+spec:
+  volumes:
+    - name: mc-world-storage
+      persistentVolumeClaim:
+        claimName: mc-claim-java
+  containers:
+    - name: mc-server-container-java
+      image: itzg/minecraft-server
+      ports:
+        - containerPort: 25565
+          name: "mc-server"
+      volumeMounts:
+        - mountPath: "/data"
+          name: mc-world-storage
+      env:
+      - name: EULA
+        value: "true"
+      - name: VERSION
+        value: '"${release}"'
+      - name: TYPE
+        value: '"${type}"'
+
+---
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: mc-exposer-java
+  labels:
+    app: java
+spec:
+  type: LoadBalancer
+  ports:
+    - protocol: TCP
+      port: 25565
+      targetPort: 25565
+  selector: 
+    app: java
+
+---
+
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: mc-claim-java
+spec:
+  accessModes:
+    - ReadWriteOnce
+  volumeMode: Filesystem
+  resources:
+    requests:
+      storage: 5G' > ./mc-pod-provisioned.yaml
+
+
+
+if [ $bedrock ]
+then 
+    release="1.12.0.28"
+  echo "This command will create a Bedrock version '"${release}"' world titled '"${worldname}"'.
+Continue(y or n)?"
+    read run
+    if [ $run = y ]
+    then
+      echo "Then here we go!"
+
+
+
+    #ACTUAL RUN SCRIPT FOR A BEDROCK SERVER
+    gcloud container clusters get-credentials $clustername --zone $zone --project $project
+
+    echo -e "server-name=Alexs K8S Server\n\
+gamemode="${gamemode}"\n\
+difficulty=normal\n\
+allow-cheats=false\n\
+max-players=10\n\
+online-mode=true\n\
+white-list=false\n\
+server-port=19132\n\
+server-portv6=19133\n\
+view-distance=32\n\
+tick-distance=4\n\
+player-idle-timeout=30\n\
+max-threads=8\n\
+level-name="${worldname}"\n\
+level-seed=\n\
+default-player-permission-level=operator\n\
+texturepack-required=false" > resources/server.properties.provisioned
+
+    echo 'kind: Pod
+apiVersion: v1
+metadata:
+  name: mc-server-pod-bedrock
+  labels: 
+    app: bedrock
+spec:
+  volumes:
+    - name: mc-world-storage
+      persistentVolumeClaim:
+        claimName: mc-claim-bedrock
+  containers:
+    - name: mc-server-container
+      image: itzg/minecraft-bedrock-server
+      ports:
+        - containerPort: 19132
+          name: "mc-server"
+      volumeMounts:
+        - mountPath: "/data"
+          name: mc-world-storage
+      env:
+      - name: EULA
+        value: "true"
+      - name: VERSION
+        value: '"${release}"'
+
+---
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: mc-exposer-toast
+  labels:
+    app: minecraft
+    world: toast
+spec:
+  type: LoadBalancer
+  ports:
+    - protocol: UDP
+      port: 19132
+      targetPort: 19132
+  selector: 
+    app: bedrock
+
+---
+
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: mc-claim-bedrock
+spec:
+  accessModes:
+    - ReadWriteOnce
+  volumeMode: Filesystem
+  resources:
+    requests:
+      storage: 5G' > ./resources/mc-pod-provisioned.yaml
+
+    gcloud compute disks create disk-java --zone us-west1-a 2> errors.txt
+    kubectl apply -f ./resources/mc-pod-provisioned.yaml
+
+    sleep 120
+
+    kubectl cp resources/server.properties mc-server-pod-bedrock:/minecraft/server.properties
+    kubectl cp resources/server_setup.sh mc-server-pod-bedrock:/server_setup.sh
+
+    kubectl exec mc-server-pod-bedrock bash /server_setup.sh
+
+
+
+    else
+      echo "Server creation cancelled"
+    fi
+else
+  if [ $modded ]
+  then
+    echo "This command will create a Forge-modded version "${release}" world titled '"${worldname}"' with the mod at
+"${modpath}" 
+installed. Continue(y or n)?"
+    read run
+    if [ $run = y ]
+    then
+      echo "Then here we go!"
+
+
+  # ACTUAL RUN SCRIPT FOR MODDED SERVER
+      gcloud container clusters get-credentials $clustername --zone us-west1-a --project terraform-gcp-harbor
+
+      gcloud compute disks create disk-java --zone us-west1-a 2> errors.txt
+      kubectl apply -f ./resources/mc-pod-provisioned.yaml
+
+      sleep 180
+
+      kubectl cp $modpath mc-server-pod-java:/data/mods/
+      kubectl cp resources/java.server.properties mc-server-pod-java:/data/server.properties
+      kubectl exec mc-server-pod-java chmod 777 server.properties
+      kubectl exec mc-server-pod-java rcon-cli stop
+
+      echo "Creation complete, please wait for the server to configure.
+Server IP Address: "
+      kubectl get svc mc-exposer-java -o jsonpath="{.status.loadBalancer.ingress[*].ip}"
+      echo ""
+
+
+
+    else
+      echo "Server creation cancelled"
+    fi
+  else 
+    if [ $ftb ]
+    then 
+      echo "This command will create a FeedTheBeast version "${release}" world titled '"${worldname}"' with the modpack at
+"${modpack}" 
+installed. Continue(y or n)?"
+      read run
+      if [ $run = y ]
+      then
+        echo "Then here we go!"
+
+
+  # ACTUAL RUN SCRIPT FOR FTB SERVER
+        
+        gcloud container clusters get-credentials $clustername --zone us-west1-a --project terraform-gcp-harbor
+
+        gcloud compute disks create disk-java --zone us-west1-a 2> errors.txt
+        kubectl apply -f ./resources/mc-pod-provisioned.yaml
+
+        sleep 400
+
+        kubectl cp resources/server.properties.provisioned mc-server-pod-java:/data/FeedTheBeast/server.properties
+        kubectl exec mc-server-pod-java chmod 777 /data/FeedTheBeast/server.properties
+        kubectl exec mc-server-pod-java rcon-cli stop
+
+        echo "Creation complete, please wait for the server to configure.
+Server IP Address: "
+        kubectl get svc mc-exposer-java -o jsonpath="{.status.loadBalancer.ingress[*].ip}"
+        echo ""
+
+
+
+      else
+        echo "Server creation cancelled"
+      fi
+    else 
+      echo "This command will create a vanilla version "${release}" world titled '"${worldname}".' Continue(y or n)?"
+      read run
+      if [ $run = y ]
+      then
+        echo "Then here we go!"
+
+
+  # ACTUAL RUN SCRIPT FOR VANILLA SERVER
+        gcloud container clusters get-credentials $clustername --zone us-west1-a --project terraform-gcp-harbor
+
+        gcloud compute disks create disk-java --zone us-west1-a 2> errors.txt
+        kubectl apply -f ./resources/mc-pod-provisioned.yaml
+
+        
+
+        sleep 150
+
+        kubectl cp resources/server.properties.provisioned mc-server-pod-java:/data/server.properties
+        kubectl exec mc-server-pod-java chmod 777 server.properties
+        kubectl exec mc-server-pod-java rcon-cli stop
+
+        echo "Creation complete, please wait for the server to configure.
+Server IP Address: "
+        kubectl get svc mc-exposer-java -o jsonpath="{.status.loadBalancer.ingress[*].ip}"
+        echo ""
+
+
+
+      else
+        echo "Server creation cancelled"
+      fi
+    fi
+  fi
+fi
+
+rm mc-pod-provisioned.yaml
+rm server.properties.provisioned
---
+++ states/default-bedrock.tfvars
@@ -0,0 +1,26 @@
+region = "us-west1"
+
+cluster-name = "mc-server"
+
+cluster-size = 1
+
+network = "minecraft"
+
+subnet = "minecraft-1"
+
+credentials-file = "~/terraform/terraform_keys/terraform-gcp-harbor-2-3ca67fec4859.json"
+
+project = "terraform-gcp-harbor-2"
+
+machine-type = "n1-standard-2"
+
+
+
+#server variables
+java = 0
+
+bedrock = 1
+
+gamemode = "creative"
+
+worldname = "GKE"
