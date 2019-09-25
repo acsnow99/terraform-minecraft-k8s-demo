1688067e92255a1454cf9b4342c947e7e0894d17
Alex Snow <asnow@ignw.io>
2019-09-25 14:53:15 -0700

    Variablized Jenkins, changed to only work with my Jenkins

---
+++ resources/provision-jenkins.sh
@@ -0,0 +1,9 @@
+cat resources/mc-pod-jenkins.yaml | sed "s/${RELEASE}/$RELEASE/g" \
+| sed "s/${SERVERTYPE}/$SERVERTYPE/g" \
+| sed "s/${MODPACK}/$MODPACK/g" \
+| sed "s/${DOCKER_REPO}/$DOCKER_REPO/g" \
+> resources/deployment.yaml
+cat resources/pvc-jenkins.yaml > resources/pvc-deployment.yaml
+cat resources/server.properties.jenkins | sed "s/${WORLDNAME}/$WORLDNAME/g" \
+| sed "s/${GAMEMODE}/$GAMEMODE/g" \
+> resources/server.properties.provisioned
